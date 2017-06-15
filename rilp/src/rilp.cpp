070d60c2dca6989c81e213431ee5db78ffadacd7
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2017-06-15 14:33:35 +0200

    R: add a morris sensitivity package example

--- rilp/src/rilp.cpp
+++ rilp/src/rilp.cpp
@@ -26,20 +26,19 @@
 
 using namespace Rcpp;
 
-/*
- * Convert the lp::result_status enumeration to an integer reusabe in R
- * code.
- */
-constexpr int status_get(lp::result_status status) noexcept;
-
 //' Tries to solve the 01 linear programming problem.
 //'
-//' @return A list:
-//' - Integer: 0: internal_error, 1: success, 2: time limit reached, 3:
-//'   kappa max reched and 4: loop limit reached.
-//' - Real: the solution found.
-//' - Real: the duration to found a solution.
-//' - Integer: the constraints remaining if a limit is reached.
+//' @param constraint_order: 0-none, 1-reversing, 2-random-sorting,
+//' 3-infeasibility-decr, 4- infeasibility-incr
+//'
+//' @return A list of two value: The remaining constraint and the value of
+//'   the solution (Integer, Real):
+//'   - Integer: The number of remaining constraints. 0 means a solution
+//'      found, greater than 0 means remaining constraints and value is
+//'      0. -1 means an error occured during solving. Use the @c `lp`
+//'      program to check out the error.
+//'   - Real: The value of the solution found (if the remaining
+//'     constraint equal 0).
 //'
 //' @useDynLib rilp
 //' @importFrom Rcpp sourceCpp
@@ -51,7 +50,7 @@ solve_01lp_problem(std::string file_path,
                    long int limit = 1000,
                    double theta = 0.5,
                    double delta = 1e-4,
-                   std::string constraint_order = "none",
+                   long int constraint_order = 0,
                    double kappa_min = 0.1,
                    double kappa_step = 1e-4,
                    double kappa_max = 1.0,
@@ -62,63 +61,65 @@ solve_01lp_problem(std::string file_path,
                    long int pushes_limit = 50,
                    double pushing_objective_amplifier = 10,
                    long int pushing_iteration_limit = 10,
+                   long int seed = -1,
                    long int thread = 1) noexcept
 {
     try {
         auto pb = lp::make_problem(file_path);
 
         std::map<std::string, lp::parameter> params;
-        params["limit"] = limit, params["theta"] = theta;
+        params["limit"] = limit;
+        params["theta"] = theta;
         params["delta"] = delta;
+
         params["kappa-min"] = kappa_min;
         params["kappa-step"] = kappa_step;
         params["kappa-max"] = kappa_max;
         params["alpha"] = alpha;
-        params["constraint-order"] = constraint_order;
         params["w"] = w;
+
+        if (seed > 0)
+            params["seed"] = seed;
+
+        switch (constraint_order) {
+        case 1:
+            params["constraint-order"] = std::string("reversing");
+            break;
+        case 2:
+            params["constraint-order"] = std::string("random-sorting");
+            break;
+        case 3:
+            params["constraint-order"] = std::string("infeasibility-decr");
+            break;
+        case 4:
+            params["constraint-order"] = std::string("infeasibility-incr");
+            break;
+        case 0:
+        default:
+            params["constraint-order"] = std::string("none");
+            break;
+        }
 
         params["time-limit"] = time_limit;
         params["pushing-k-factor"] = pushing_k_factor;
         params["pushes-limit"] = pushes_limit;
         params["pushing-objective-amplifier"] = pushing_objective_amplifier;
         params["pushing-iteration-limit"] = pushing_iteration_limit;
-
         params["thread"] = thread;
 
         auto result = lp::solve(pb, params);
 
-        return List::create(status_get(result.status),
-                            result.value,
-                            result.duration,
-                            result.remaining_constraints);
+        if (result.status == lp::result_status::success)
+            return List::create(result.remaining_constraints, result.value);
 
+        return List::create(result.remaining_constraints, NA_REAL);
     } catch (const std::bad_alloc& e) {
-        Rcpp::Rcout << "lp: " << e.what() << '\n';
+        Rcpp::Rcout << "lp memory error: " << e.what() << '\n';
     } catch (const std::exception& e) {
-        Rcpp::Rcout << "lp: " << e.what() << '\n';
+        Rcpp::Rcout << "lp error: " << e.what() << '\n';
     } catch (...) {
-        Rcpp::Rcout << "lp: unknown error\n";
+        Rcpp::Rcout << "lp error: unknown error\n";
     }
 
-    return List::create();
-}
-
-constexpr int
-status_get(lp::result_status status) noexcept
-{
-    switch (status) {
-    case lp::result_status::success:
-        return 1;
-    case lp::result_status::time_limit_reached:
-        return 2;
-    case lp::result_status::kappa_max_reached:
-        return 3;
-        break;
-    case lp::result_status::limit_reached:
-        return 4;
-        break;
-    default:
-        return 0;
-        break;
-    }
+    return List::create(NA_INTEGER, NA_REAL);
 }
--- rilp/tests/solve.R
+++ rilp/tests/solve.R
@@ -1,4 +1,55 @@
 library(rilp)
+library(sensitivity)
 
-result = rilp::solve_01lp_problem("uf50-0448.lp")
-result
+factors=c("theta", "delta", "constraint_order", "kappa_min",
+                   "kappa_step", "kappa_max", "alpha", "w")
+bounds = data.frame(min=c(0,    0, 0, 0.0,   0,  1.0, 0.0,    0),
+                    max=c(1, 1e10, 4, 1.0, 0.1, 10.0, 2.0, 1000))
+rownames(bounds) <- factors
+
+morrisDesign <- morris(model = NULL,
+                factors = factors,
+                r = 100,
+                design=list(type="oat", levels=10, grid.jump=5),
+                binf = bounds$min,
+                bsup = bounds$max,
+                scale=TRUE)
+
+# morrisDesign$X
+#
+# 20 * (8 + 1)
+#
+
+f <- function(x, thread=1, limit=1000000, time_limit=1) {
+  theta <- x["theta"]
+  delta <- x["delta"]
+  constraint_order <- x["constraint_order"]
+  kappa_min <- x["kappa_min"]
+  kappa_step <- x["kappa_step"]
+  kappa_max <- x["kappa_max"]
+  alpha <- x["alpha"]
+  w <- x["w"]
+
+  r <- rilp::solve_01lp_problem(file_path="n-queens-problem-20.lp",
+           limit = limit,
+           theta = theta,
+           delta = delta,
+           constraint_order = constraint_order,
+           kappa_min = kappa_min,
+           kappa_step = kappa_step,
+           kappa_max = kappa_max,
+           alpha = alpha,
+           w = w,
+           time_limit = time_limit,
+           seed = 123654785,
+           thread = 1L)
+
+  return(r)
+}
+
+r = apply(morrisDesign$X, 1, f)
+mr = matrix(unlist(r), ncol=2, byrow = TRUE)
+mr1 <- mr[,-2]
+
+tell(morrisDesign, mr1)
+plot(morrisDesign)
