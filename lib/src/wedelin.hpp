552c961da5fcc5eb2eb2f59debbe87835c9516b6
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2017-07-27 14:30:00 +0200

    solver: remove generalized/default wedelin solvers

--- lib/src/wedelin.hpp
+++ lib/src/wedelin.hpp
@@ -25,281 +25,285 @@
 
 #include "mitm.hpp"
 #include "utils.hpp"
-#include <Eigen/Core>
+
 #include <iostream>
 #include <iterator>
 
 namespace lp {
-namespace details {
 
-Eigen::MatrixXi
-make_a(index m, index n, const problem& pb)
-{
-    Eigen::MatrixXi A = Eigen::MatrixXi::Zero(m, n);
+// namespace details {
 
-    for (lp::index i = 0; i != m; ++i) {
-        for (const auto& elem : pb.equal_constraints[i].elements) {
-            auto j = elem.variable_index;
-            A(i, j) = 1;
-        }
-    }
+// Eigen::MatrixXi
+// make_a(index m, index n, const problem& pb)
+// {
+//     Eigen::MatrixXi A = Eigen::MatrixXi::Zero(m, n);
 
-    return A;
-}
+//     for (lp::index i = 0; i != m; ++i) {
+//         for (const auto& elem : pb.equal_constraints[i].elements) {
+//             auto j = elem.variable_index;
+//             A(i, j) = 1;
+//         }
+//     }
 
-Eigen::VectorXi
-make_b(index m, const problem& p)
-{
-    Eigen::VectorXi b = Eigen::VectorXi::Zero(m);
+//     return A;
+// }
 
-    for (lp::index i = 0; i != m; ++i)
-        b(i) = p.equal_constraints[i].value;
+// Eigen::VectorXi
+// make_b(index m, const problem& p)
+// {
+//     Eigen::VectorXi b = Eigen::VectorXi::Zero(m);
 
-    return b;
-}
+//     for (lp::index i = 0; i != m; ++i)
+//         b(i) = p.equal_constraints[i].value;
 
-Eigen::VectorXf
-make_c(index n, const problem& p)
-{
-    Eigen::VectorXf c = Eigen::VectorXf::Zero(n);
+//     return b;
+// }
 
-    for (const auto& elem : p.objective.elements)
-        c(elem.variable_index) = elem.factor;
+// Eigen::VectorXf
+// make_c(index n, const problem& p)
+// {
+//     Eigen::VectorXf c = Eigen::VectorXf::Zero(n);
 
-    return c;
-}
+//     for (const auto& elem : p.objective.elements)
+//         c(elem.variable_index) = elem.factor;
 
-struct maximize_wedelin_tag
-{
-};
-struct minimize_wedelin_tag
-{
-};
+//     return c;
+// }
 
-template <typename T>
-struct solver_wedelin_tag
-{
-    using type = T;
-};
+// struct maximize_wedelin_tag
+// {
+// };
+// struct minimize_wedelin_tag
+// {
+// };
 
-template <typename iteratorT>
-void
-sort_wedelin(iteratorT begin, iteratorT end, minimize_wedelin_tag)
-{
-    std::sort(begin, end, [](const auto& lhs, const auto& rhs) {
-        return lhs.value < rhs.value;
-    });
-}
+// template <typename T>
+// struct solver_wedelin_tag
+// {
+//     using type = T;
+// };
 
-template <typename iteratorT>
-void
-sort_wedelin(iteratorT begin, iteratorT end, maximize_wedelin_tag)
-{
-    std::sort(begin, end, [](const auto& lhs, const auto& rhs) {
-        return rhs.value < lhs.value;
-    });
-}
+// template <typename iteratorT>
+// void
+// sort_wedelin(iteratorT begin, iteratorT end, minimize_wedelin_tag)
+// {
+//     std::sort(begin, end, [](const auto& lhs, const auto& rhs) {
+//         return lhs.value < rhs.value;
+//     });
+// }
 
-template <typename modeT>
-class default_algorithm
-{
-    struct r_data
-    {
-        r_data(double value_, index index_)
-          : value(value_)
-          , id(index_)
-        {
-        }
+// template <typename iteratorT>
+// void
+// sort_wedelin(iteratorT begin, iteratorT end, maximize_wedelin_tag)
+// {
+//     std::sort(begin, end, [](const auto& lhs, const auto& rhs) {
+//         return rhs.value < lhs.value;
+//     });
+// }
 
-        double value;
-        index id;
-    };
+// template <typename modeT>
+// class default_algorithm
+// {
+//     struct r_data
+//     {
+//         r_data(double value_, index index_)
+//           : value(value_)
+//           , id(index_)
+//         {
+//         }
 
-    using tag = modeT;
-    index m, n;
-    const problem& pb;
-    Eigen::MatrixXi A;
-    Eigen::VectorXi b;
-    Eigen::VectorXf c;
-    Eigen::VectorXi x;
-    Eigen::MatrixXf P;
-    Eigen::VectorXf pi;
-    std::vector<std::vector<index>> I;
-    std::vector<std::vector<r_data>> r;
-    double kappa, delta, theta;
-    index loop;
-    bool solution_found;
+//         double value;
+//         index id;
+//     };
 
-    void update_row(int k)
-    {
-        for (auto i : I[k])
-            P(k, i) *= theta;
+//     using tag = modeT;
+//     index m, n;
+//     const problem& pb;
+//     Eigen::MatrixXi A;
+//     Eigen::VectorXi b;
+//     Eigen::VectorXf c;
+//     Eigen::VectorXi x;
+//     Eigen::MatrixXf P;
+//     Eigen::VectorXf pi;
+//     std::vector<std::vector<index>> I;
+//     std::vector<std::vector<r_data>> r;
+//     double kappa, delta, theta;
+//     index loop;
+//     bool solution_found;
 
-        for (index i = 0, endi = numeric_cast<index>(I[k].size()); i != endi;
-             ++i) {
-            double sum_a_pi{ 0 };
-            double sum_a_p{ 0 };
+//     void update_row(int k)
+//     {
+//         for (auto i : I[k])
+//             P(k, i) *= theta;
 
-            assert(k < numeric_cast<index>(I.size()));
-            assert(i < numeric_cast<index>(I[k].size()));
+//         for (index i = 0, endi = numeric_cast<index>(I[k].size()); i !=
+//         endi;
+//              ++i) {
+//             double sum_a_pi{ 0 };
+//             double sum_a_p{ 0 };
 
-            for (index h = 0; h != m; ++h) {
-                if (A(h, I[k][i])) {
-                    sum_a_pi += A(h, I[k][i]) * pi(h);
-                    sum_a_p += A(h, I[k][i]) * P(h, I[k][i]);
-                }
-            }
+//             assert(k < numeric_cast<index>(I.size()));
+//             assert(i < numeric_cast<index>(I[k].size()));
 
-            r[k][i].value = c(I[k][i]) - sum_a_pi - sum_a_p;
-            r[k][i].id = I[k][i];
-        }
+//             for (index h = 0; h != m; ++h) {
+//                 if (A(h, I[k][i])) {
+//                     sum_a_pi += A(h, I[k][i]) * pi(h);
+//                     sum_a_p += A(h, I[k][i]) * P(h, I[k][i]);
+//                 }
+//             }
 
-        details::sort_wedelin(r[k].begin(), r[k].end(), tag());
+//             r[k][i].value = c(I[k][i]) - sum_a_pi - sum_a_p;
+//             r[k][i].id = I[k][i];
+//         }
 
-        pi(k) += ((r[k][b(k)].value + r[k][b(k) - 1].value) / 2.0);
+//         details::sort_wedelin(r[k].begin(), r[k].end(), tag());
 
-        double d = delta + ((kappa / (1.0 - kappa)) *
-                            (r[k][b(k) - 1].value - r[k][b(k)].value));
+//         pi(k) += ((r[k][b(k)].value + r[k][b(k) - 1].value) / 2.0);
 
-        for (index j = 0; j < b(k); ++j) {
-            x(r[k][j].id) = 1;
-            P(k, r[k][j].id) -= d;
-        }
+//         double d = delta + ((kappa / (1.0 - kappa)) *
+//                             (r[k][b(k) - 1].value - r[k][b(k)].value));
 
-        for (index j = b(k), endj = numeric_cast<index>(I[k].size());
-             j != endj;
-             ++j) {
-            x(r[k][j].id) = 0;
-            P(k, r[k][j].id) -= -d;
-        }
-    }
+//         for (index j = 0; j < b(k); ++j) {
+//             x(r[k][j].id) = 1;
+//             P(k, r[k][j].id) -= d;
+//         }
 
-public:
-    default_algorithm(double kappa_,
-                      double delta_,
-                      double theta_,
-                      long limit,
-                      const problem& pb_)
-      : m(std::distance(pb_.equal_constraints.begin(),
-                        pb_.equal_constraints.end()))
-      , n(std::distance(pb_.vars.values.begin(), pb_.vars.values.end()))
-      , pb(pb_)
-      , A(make_a(m, n, pb))
-      , b(make_b(m, pb))
-      , c(make_c(n, pb))
-      , x(Eigen::VectorXi::Zero(n))
-      , P(Eigen::MatrixXf::Zero(m, n))
-      , pi(Eigen::VectorXf::Zero(m))
-      , I(m)
-      , r(m)
-      , kappa(kappa_)
-      , delta(delta_)
-      , theta(theta_)
-      , loop(0)
-      , solution_found(false)
-    {
-        Ensures(kappa >= 0 and kappa < 1, "kappa [0, 1[");
-        Ensures(delta >= 0, "delta [0, +oo[");
-        Ensures(theta >= 0 and theta <= 1, "theta [0, 1]");
-        Ensures(m > 0, "equal_constraints number must be > 0");
-        Ensures(n > 0, "variable number must be > 0");
+//         for (index j = b(k), endj = numeric_cast<index>(I[k].size());
+//              j != endj;
+//              ++j) {
+//             x(r[k][j].id) = 0;
+//             P(k, r[k][j].id) -= -d;
+//         }
+//     }
 
-        for (const auto& elem : pb.objective.elements)
-            x(elem.variable_index) = c(elem.variable_index) <= 0;
+// public:
+//     default_algorithm(double kappa_,
+//                       double delta_,
+//                       double theta_,
+//                       long limit,
+//                       const problem& pb_)
+//       : m(std::distance(pb_.equal_constraints.begin(),
+//                         pb_.equal_constraints.end()))
+//       , n(std::distance(pb_.vars.values.begin(), pb_.vars.values.end()))
+//       , pb(pb_)
+//       , A(make_a(m, n, pb))
+//       , b(make_b(m, pb))
+//       , c(make_c(n, pb))
+//       , x(Eigen::VectorXi::Zero(n))
+//       , P(Eigen::MatrixXf::Zero(m, n))
+//       , pi(Eigen::VectorXf::Zero(m))
+//       , I(m)
+//       , r(m)
+//       , kappa(kappa_)
+//       , delta(delta_)
+//       , theta(theta_)
+//       , loop(0)
+//       , solution_found(false)
+//     {
+//         Ensures(kappa >= 0 and kappa < 1, "kappa [0, 1[");
+//         Ensures(delta >= 0, "delta [0, +oo[");
+//         Ensures(theta >= 0 and theta <= 1, "theta [0, 1]");
+//         Ensures(m > 0, "equal_constraints number must be > 0");
+//         Ensures(n > 0, "variable number must be > 0");
 
-        for (index i = 0; i != m; ++i) {
-            for (const auto& elem : pb.equal_constraints[i].elements) {
-                I[i].emplace_back(elem.variable_index);
-                r[i].emplace_back(0.0, elem.variable_index);
-            }
-        }
+//         for (const auto& elem : pb.objective.elements)
+//             x(elem.variable_index) = c(elem.variable_index) <= 0;
 
-        // std::cout << "A:\n" << A << '\n'
-        //           << "b:\n" << b.transpose() << '\n'
-        //           << "c:\n" << c.transpose() << '\n'
-        //           << "x:\n" << x.transpose() << '\n'
-        //           << '\n';
+//         for (index i = 0; i != m; ++i) {
+//             for (const auto& elem : pb.equal_constraints[i].elements) {
+//                 I[i].emplace_back(elem.variable_index);
+//                 r[i].emplace_back(0.0, elem.variable_index);
+//             }
+//         }
 
-        std::vector<index> R;
-        while (loop != limit) {
-            for (index k{ 0 }; k != m; ++k) {
-                int v = 0;
-                for (index i{ 0 }; i != n; ++i)
-                    v += A(k, i) * x(i);
+//         // std::cout << "A:\n" << A << '\n'
+//         //           << "b:\n" << b.transpose() << '\n'
+//         //           << "c:\n" << c.transpose() << '\n'
+//         //           << "x:\n" << x.transpose() << '\n'
+//         //           << '\n';
 
-                if (v != b(k))
-                    R.push_back(k);
-            }
+//         std::vector<index> R;
+//         while (loop != limit) {
+//             for (index k{ 0 }; k != m; ++k) {
+//                 int v = 0;
+//                 for (index i{ 0 }; i != n; ++i)
+//                     v += A(k, i) * x(i);
 
-            if (R.empty()) {
-                solution_found = true;
-                return;
-            }
+//                 if (v != b(k))
+//                     R.push_back(k);
+//             }
 
-            // std::cout << "P:\n" << P << '\n';
-            // std::cout << "pi:\n" << pi.transpose() << '\n';
+//             if (R.empty()) {
+//                 solution_found = true;
+//                 return;
+//             }
 
-            for (auto k : R)
-                update_row(k);
+//             // std::cout << "P:\n" << P << '\n';
+//             // std::cout << "pi:\n" << pi.transpose() << '\n';
 
-            R.clear();
-            ++loop;
-        }
-    }
+//             for (auto k : R)
+//                 update_row(k);
 
-    double compute_value() const
-    {
-        double ret{ 0 };
+//             R.clear();
+//             ++loop;
+//         }
+//     }
 
-        for (auto& elem : pb.objective.elements)
-            ret += elem.factor * x(elem.variable_index);
+//     double compute_value() const
+//     {
+//         double ret{ 0 };
 
-        return ret;
-    }
+//         for (auto& elem : pb.objective.elements)
+//             ret += elem.factor * x(elem.variable_index);
 
-    result results() const
-    {
-        result ret;
-        ret.method = "wedelin";
-        ret.loop = loop;
+//         return ret;
+//     }
 
-        if (solution_found)
-            ret.status = result_status::success;
-        ret.value = compute_value();
-        ret.variable_name.resize(n);
-        ret.variable_value.resize(n, 0);
+//     result results() const
+//     {
+//         result ret;
+//         ret.method = "wedelin";
+//         ret.loop = loop;
 
-        for (index i = 0; i != n; ++i) {
-            ret.variable_name[i] = pb.vars.names[i];
-            if (x(i))
-                ret.variable_value[i] = true;
-        }
+//         if (solution_found)
+//             ret.status = result_status::success;
+//         ret.value = compute_value();
+//         ret.variable_name.resize(n);
+//         ret.variable_value.resize(n, 0);
 
-        return ret;
-    }
-};
+//         for (index i = 0; i != n; ++i) {
+//             ret.variable_name[i] = pb.vars.names[i];
+//             if (x(i))
+//                 ret.variable_value[i] = true;
+//         }
 
-} // namespace details
+//         return ret;
+//     }
+// };
+
+// } // namespace details
 
 inline result
-simple_wedelin(double kappa,
-               double delta,
-               double theta,
-               long limit,
-               const problem& pb)
+simple_wedelin(double /* kappa */,
+               double /* delta */,
+               double /* theta */,
+               long /* limit */,
+               const problem& /* pb */)
 {
-    using namespace details;
+    // using namespace details;
 
-    using maximize_solver = default_algorithm<maximize_wedelin_tag>;
-    using minimize_solver = default_algorithm<minimize_wedelin_tag>;
+    // using maximize_solver = default_algorithm<maximize_wedelin_tag>;
+    // using minimize_solver = default_algorithm<minimize_wedelin_tag>;
 
-    if (pb.type == lp::objective_function_type::maximize) {
-        maximize_solver solver(kappa, delta, theta, limit, pb);
-        return solver.results();
-    }
+    // if (pb.type == lp::objective_function_type::maximize) {
+    //     maximize_solver solver(kappa, delta, theta, limit, pb);
+    //     return solver.results();
+    // }
 
-    minimize_solver solver(kappa, delta, theta, limit, pb);
-    return solver.results();
+    // minimize_solver solver(kappa, delta, theta, limit, pb);
+    // return solver.results();
+
+    return lp::result{};
 }
 
 } // namespace lp
