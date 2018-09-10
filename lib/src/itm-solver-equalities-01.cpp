c910a49c9764eb8bec7f8e93bb51e83139beaea4
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2018-09-10 12:12:41 +0200

    itm: merge hpp and cpp solver files

--- lib/src/itm-solver-equalities-01.cpp
+++ lib/src/itm-solver-equalities-01.cpp
@@ -20,34 +20,299 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include "itm-solver-equalities-01.hpp"
-
+#include "itm-common.hpp"
 #include "itm-optimizer-common.hpp"
 #include "itm-solver-common.hpp"
 
 namespace baryonyx {
 namespace itm {
 
-#if 0
-template struct solver_equalities_01coeff<float,
-                                           minimize_tag,
-                                           std::default_random_engine>;
-template struct solver_equalities_01coeff<double,
-                                           minimize_tag,
-                                           std::default_random_engine>;
-template struct solver_equalities_01coeff<long double,
-                                           minimize_tag,
-                                           std::default_random_engine>;
-template struct solver_equalities_01coeff<float,
-                                           maximize_tag,
-                                           std::default_random_engine>;
-template struct solver_equalities_01coeff<double,
-                                           maximize_tag,
-                                           std::default_random_engine>;
-template struct solver_equalities_01coeff<long double,
-                                           maximize_tag,
-                                           std::default_random_engine>;
-#endif
+template<typename Float, typename Mode, typename Random>
+struct solver_equalities_01coeff
+{
+    using mode_type = Mode;
+    using float_type = Float;
+
+    Random& rng;
+
+    sparse_matrix<int> ap;
+    std::unique_ptr<Float[]> P;
+    std::unique_ptr<r_data<Float>[]> R;
+    std::unique_ptr<int[]> b;
+    std::unique_ptr<Float[]> pi;
+
+    const std::unique_ptr<Float[]>& c;
+
+    int m;
+    int n;
+
+    solver_equalities_01coeff(Random& rng_,
+                              int m_,
+                              int n_,
+                              const std::unique_ptr<Float[]>& c_,
+                              const std::vector<merged_constraint>& csts)
+      : rng(rng_)
+      , ap(csts, m_, n_)
+      , P(std::make_unique<Float[]>(ap.size()))
+      , R(std::make_unique<r_data<Float>[]>(
+          compute_reduced_costs_vector_size(csts)))
+      , b(std::make_unique<int[]>(m_))
+      , pi(std::make_unique<Float[]>(m_))
+      , c(c_)
+      , m(m_)
+      , n(n_)
+    {
+        for (int i = 0; i != m; ++i) {
+            for (const auto& cst : csts[i].elements)
+                bx_ensures(cst.factor == 1);
+
+            bx_ensures(csts[i].min == csts[i].max);
+
+            b[i] = csts[i].min;
+        }
+    }
+
+    int factor(int /*value*/) const noexcept
+    {
+        return 1;
+    }
+
+    int bound_min(int constraint) const noexcept
+    {
+        return b[constraint];
+    }
+
+    int bound_max(int constraint) const noexcept
+    {
+        return b[constraint];
+    }
+
+    int bound_init(int constraint) const
+    {
+        return b[constraint];
+    }
+
+    Float compute_sum_A_pi(int variable) const
+    {
+        Float ret{ 0 };
+
+        sparse_matrix<int>::const_col_iterator ht, hend;
+        std::tie(ht, hend) = ap.column(variable);
+
+        for (; ht != hend; ++ht)
+            ret += pi[ht->row];
+
+        return ret;
+    }
+
+    template<typename Xtype>
+    bool is_valid_solution(const Xtype& x) const
+    {
+        for (int k = 0; k != m; ++k) {
+            sparse_matrix<int>::const_row_iterator it, et;
+
+            std::tie(it, et) = ap.row(k);
+            int v = 0;
+
+            for (; it != et; ++it)
+                v += x[it->column];
+
+            if (b[k] != v)
+                return false;
+        }
+
+        return true;
+    }
+
+    template<typename Xtype>
+    int compute_violated_constraints(const Xtype& x,
+                                     std::vector<int>& container) const
+    {
+        typename sparse_matrix<int>::const_row_iterator it, et;
+
+        container.clear();
+
+        for (int k = 0; k != m; ++k) {
+            std::tie(it, et) = ap.row(k);
+            int v = 0;
+
+            for (; it != et; ++it)
+                v += x[it->column];
+
+            if (b[k] != v)
+                container.emplace_back(k);
+        }
+
+        return length(container);
+    }
+
+    template<typename Xtype>
+    double results(const Xtype& x,
+                   const std::unique_ptr<Float[]>& original_costs,
+                   const double cost_constant) const
+    {
+        bx_expects(is_valid_solution(x));
+
+        auto value = static_cast<double>(cost_constant);
+
+        for (int i{ 0 }, ei{ n }; i != ei; ++i)
+            value += static_cast<double>(original_costs[i] * x[i]);
+
+        return value;
+    }
+
+    //
+    // Decrease influence of local preferences. 0 will completely reset the
+    // preference values for the current row. > 0 will keep former decision
+    // in mind.
+    //
+    void decrease_preference(sparse_matrix<int>::row_iterator begin,
+                             sparse_matrix<int>::row_iterator end,
+                             Float theta) noexcept
+    {
+        for (; begin != end; ++begin)
+            P[begin->value] *= theta;
+    }
+
+    //
+    // Compute the reduced costs and return the size of the newly R vector.
+    //
+    int compute_reduced_costs(sparse_matrix<int>::row_iterator begin,
+                              sparse_matrix<int>::row_iterator end) noexcept
+    {
+        int r_size = 0;
+
+        for (; begin != end; ++begin) {
+            Float sum_a_pi = 0;
+            Float sum_a_p = 0;
+
+            sparse_matrix<int>::const_col_iterator ht, hend;
+            std::tie(ht, hend) = ap.column(begin->column);
+
+            for (; ht != hend; ++ht) {
+                sum_a_pi += pi[ht->row];
+                sum_a_p += P[ht->value];
+            }
+
+            R[r_size].id = r_size;
+            R[r_size].value = c[begin->column] - sum_a_pi - sum_a_p;
+            ++r_size;
+        }
+
+        return r_size;
+    }
+
+    int select_variables_equality(const int r_size, int bk)
+    {
+        bk = std::min(bk, r_size);
+
+        return bk - 1;
+    }
+
+    //
+    // The bkmin and bkmax constraint bounds are not equal and can be
+    // assigned to -infinity or +infinity. We have to scan the r vector and
+    // search a value j such as b(0, k) <= Sum A(k, R[j]) < b(1, k).
+    //
+    template<typename Xtype>
+    void affect_variables(Xtype& x,
+                          sparse_matrix<int>::row_iterator it,
+                          int k,
+                          int selected,
+                          int r_size,
+                          const Float kappa,
+                          const Float delta) noexcept
+    {
+        if (selected < 0) {
+            for (int i = 0; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, false);
+                P[var->value] -= delta;
+            }
+        } else if (selected + 1 >= r_size) {
+            pi[k] += R[selected].value;
+
+            for (int i = 0; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, true);
+                P[var->value] += delta;
+            }
+        } else {
+            pi[k] += ((R[selected].value + R[selected + 1].value) /
+                      static_cast<Float>(2.0));
+
+            Float d = delta + ((kappa / (static_cast<Float>(1.0) - kappa)) *
+                               (R[selected + 1].value - R[selected].value));
+
+            int i = 0;
+            for (; i <= selected; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, true);
+                P[var->value] += d;
+            }
+
+            for (; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, false);
+                P[var->value] -= d;
+            }
+        }
+    }
+
+    template<typename Xtype>
+    void push_and_compute_update_row(Xtype& x,
+                                     std::vector<int>::iterator first,
+                                     std::vector<int>::iterator last,
+                                     Float kappa,
+                                     Float delta,
+                                     Float theta,
+                                     Float objective_amplifier)
+    {
+        for (; first != last; ++first) {
+            auto k = *first;
+
+            sparse_matrix<int>::row_iterator it, et;
+            std::tie(it, et) = ap.row(k);
+
+            decrease_preference(it, et, theta);
+            const int r_size = compute_reduced_costs(it, et);
+
+            if (objective_amplifier)
+                for (int i = 0; i != r_size; ++i)
+                    R[i].value += objective_amplifier * c[R[i].id];
+
+            calculator_sort(R.get(), R.get() + r_size, rng, Mode());
+            int selected = select_variables_equality(r_size, b[k]);
+            affect_variables(x, it, k, selected, r_size, kappa, delta);
+        }
+    }
+
+    template<typename Xtype>
+    void compute_update_row(Xtype& x,
+                            std::vector<int>::iterator first,
+                            std::vector<int>::iterator last,
+                            Float kappa,
+                            Float delta,
+                            Float theta)
+    {
+        for (; first != last; ++first) {
+            auto k = *first;
+
+            sparse_matrix<int>::row_iterator it, et;
+            std::tie(it, et) = ap.row(k);
+
+            decrease_preference(it, et, theta);
+            const int r_size = compute_reduced_costs(it, et);
+            calculator_sort(R.get(), R.get() + r_size, rng, Mode());
+            int selected = select_variables_equality(r_size, b[k]);
+            affect_variables(x, it, k, selected, r_size, kappa, delta);
+        }
+    }
+};
 
 template<typename Solver,
          typename Float,
--- lib/src/itm-solver-inequalities-01.cpp
+++ lib/src/itm-solver-inequalities-01.cpp
@@ -20,34 +20,386 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include "itm-solver-inequalities-01.hpp"
-
+#include "itm-common.hpp"
 #include "itm-optimizer-common.hpp"
 #include "itm-solver-common.hpp"
 
 namespace baryonyx {
 namespace itm {
 
-#if 0
-template struct solver_inequalities_01coeff<float,
-                                            minimize_tag,
-                                            std::default_random_engine>;
-template struct solver_inequalities_01coeff<double,
-                                            minimize_tag,
-                                            std::default_random_engine>;
-template struct solver_inequalities_01coeff<long double,
-                                            minimize_tag,
-                                            std::default_random_engine>;
-template struct solver_inequalities_01coeff<float,
-                                            maximize_tag,
-                                            std::default_random_engine>;
-template struct solver_inequalities_01coeff<double,
-                                            maximize_tag,
-                                            std::default_random_engine>;
-template struct solver_inequalities_01coeff<long double,
-                                            maximize_tag,
-                                            std::default_random_engine>;
-#endif
+template<typename Float, typename Mode, typename Random>
+struct solver_inequalities_01coeff
+{
+    using mode_type = Mode;
+    using float_type = Float;
+
+    Random& rng;
+
+    sparse_matrix<int> ap;
+    std::unique_ptr<Float[]> P;
+    std::unique_ptr<r_data<Float>[]> R;
+    std::unique_ptr<bound[]> b;
+    std::unique_ptr<Float[]> pi;
+
+    const std::unique_ptr<Float[]>& c;
+    int m;
+    int n;
+
+    solver_inequalities_01coeff(Random& rng_,
+                                int m_,
+                                int n_,
+                                const std::unique_ptr<Float[]>& c_,
+                                const std::vector<merged_constraint>& csts)
+      : rng(rng_)
+      , ap(csts, m_, n_)
+      , P(std::make_unique<Float[]>(ap.size()))
+      , R(std::make_unique<r_data<Float>[]>(
+          compute_reduced_costs_vector_size(csts)))
+      , b(std::make_unique<bound[]>(m_))
+      , pi(std::make_unique<Float[]>(m_))
+      , c(c_)
+      , m(m_)
+      , n(n_)
+    {
+        for (int i = 0; i != m; ++i) {
+            for (const auto& cst : csts[i].elements)
+                bx_expects(cst.factor == 1);
+
+            if (csts[i].min == csts[i].max) {
+                b[i].min = csts[i].min;
+                b[i].max = csts[i].max;
+            } else {
+                b[i].min = std::max(0, csts[i].min);
+                b[i].max = std::min(length(csts[i].elements), csts[i].max);
+            }
+        }
+    }
+
+    int factor(int /*value*/) const noexcept
+    {
+        return 1;
+    }
+
+    int bound_min(int constraint) const noexcept
+    {
+        return b[constraint].min;
+    }
+
+    int bound_max(int constraint) const noexcept
+    {
+        return b[constraint].max;
+    }
+
+    int bound_init(int constraint) const
+    {
+        return bound_init(constraint, Mode());
+    }
+
+    int bound_init(int constraint, minimize_tag) const
+    {
+        return b[constraint].min;
+    }
+
+    int bound_init(int constraint, maximize_tag) const
+    {
+        return b[constraint].max;
+    }
+
+    Float compute_sum_A_pi(int variable) const
+    {
+        Float ret{ 0 };
+
+        sparse_matrix<int>::const_col_iterator ht, hend;
+        std::tie(ht, hend) = ap.column(variable);
+
+        for (; ht != hend; ++ht)
+            ret += pi[ht->row];
+
+        return ret;
+    }
+
+    template<typename Xtype>
+    bool is_valid_solution(const Xtype& x) const
+    {
+        for (int k = 0; k != m; ++k) {
+            typename sparse_matrix<int>::const_row_iterator it, et;
+
+            std::tie(it, et) = ap.row(k);
+            int v = 0;
+
+            for (; it != et; ++it)
+                v += x[it->column];
+
+            if (!(b[k].min <= v && v <= b[k].max))
+                return false;
+        }
+
+        return true;
+    }
+
+    template<typename Xtype>
+    int compute_violated_constraints(const Xtype& x,
+                                     std::vector<int>& container) const
+    {
+        typename sparse_matrix<int>::const_row_iterator it, et;
+
+        container.clear();
+
+        for (int k = 0; k != m; ++k) {
+            std::tie(it, et) = ap.row(k);
+            int v = 0;
+
+            for (; it != et; ++it)
+                v += x[it->column];
+
+            if (!(b[k].min <= v && v <= b[k].max))
+                container.emplace_back(k);
+        }
+
+        return length(container);
+    }
+
+    template<typename Xtype>
+    double results(const Xtype& x,
+                   const std::unique_ptr<Float[]>& original_costs,
+                   const double cost_constant) const
+    {
+        bx_expects(is_valid_solution(x));
+
+        auto value = static_cast<double>(cost_constant);
+
+        for (int i{ 0 }; i != n; ++i)
+            value += static_cast<double>(original_costs[i] * x[i]);
+
+        return value;
+    }
+
+    template<typename Xtype>
+    void compute_update_row_01_eq(Xtype& x,
+                                  int k,
+                                  int bk,
+                                  Float kappa,
+                                  Float delta,
+                                  Float theta,
+                                  Float objective_amplifier)
+    {
+        typename sparse_matrix<int>::row_iterator it, et;
+        std::tie(it, et) = ap.row(k);
+
+        decrease_preference(it, et, theta);
+
+        const int r_size = compute_reduced_costs(it, et);
+
+        //
+        // Before sort and select variables, we apply the push method: for each
+        // reduces cost, we had the cost multiply with an objective amplifier.
+        //
+
+        if (objective_amplifier)
+            for (int i = 0; i != r_size; ++i)
+                R[i].value += objective_amplifier * c[(it + R[i].id)->column];
+
+        calculator_sort(R.get(), R.get() + r_size, rng, Mode());
+
+        int selected = select_variables_equality(r_size, bk);
+
+        affect_variables(x, it, k, selected, r_size, kappa, delta);
+    }
+
+    template<typename Xtype>
+    void compute_update_row_01_ineq(Xtype& x,
+                                    int k,
+                                    int bkmin,
+                                    int bkmax,
+                                    Float kappa,
+                                    Float delta,
+                                    Float theta,
+                                    Float objective_amplifier)
+    {
+        typename sparse_matrix<int>::row_iterator it, et;
+        std::tie(it, et) = ap.row(k);
+
+        decrease_preference(it, et, theta);
+
+        const int r_size = compute_reduced_costs(it, et);
+
+        //
+        // Before sort and select variables, we apply the push method: for each
+        // reduces cost, we had the cost multiply with an objective amplifier.
+        //
+
+        if (objective_amplifier)
+            for (int i = 0; i != r_size; ++i)
+                R[i].value += objective_amplifier * c[(it + R[i].id)->column];
+
+        calculator_sort(R.get(), R.get() + r_size, rng, Mode());
+
+        int selected = select_variables_inequality(r_size, bkmin, bkmax);
+
+        affect_variables(x, it, k, selected, r_size, kappa, delta);
+    }
+
+    //
+    // Decrease influence of local preferences. 0 will completely reset the
+    // preference values for the current row. > 0 will keep former decision in
+    // mind.
+    //
+    void decrease_preference(sparse_matrix<int>::row_iterator begin,
+                             sparse_matrix<int>::row_iterator end,
+                             Float theta) noexcept
+    {
+        for (; begin != end; ++begin)
+            P[begin->value] *= theta;
+    }
+
+    //
+    // Compute the reduced costs and return the size of the newly R vector.
+    //
+    int compute_reduced_costs(sparse_matrix<int>::row_iterator begin,
+                              sparse_matrix<int>::row_iterator end) noexcept
+    {
+        int r_size = 0;
+
+        for (; begin != end; ++begin) {
+            Float sum_a_pi = 0;
+            Float sum_a_p = 0;
+
+            typename sparse_matrix<int>::const_col_iterator ht, hend;
+            std::tie(ht, hend) = ap.column(begin->column);
+
+            for (; ht != hend; ++ht) {
+                sum_a_pi += pi[ht->row];
+                sum_a_p += P[ht->value];
+            }
+
+            R[r_size].id = r_size;
+            R[r_size].value = c[begin->column] - sum_a_pi - sum_a_p;
+            ++r_size;
+        }
+
+        return r_size;
+    }
+
+    int select_variables_equality(const int r_size, int bk)
+    {
+        bk = std::min(bk, r_size);
+
+        return bk - 1;
+    }
+
+    int select_variables_inequality(const int r_size, int bkmin, int bkmax)
+    {
+        bkmin = std::min(bkmin, r_size);
+        bkmax = std::min(bkmax, r_size);
+
+        for (int i = bkmin; i != bkmax; ++i)
+            if (stop_iterating(R[i].value, rng, Mode()))
+                return i - 1;
+
+        return bkmax - 1;
+    }
+
+    //
+    // The bkmin and bkmax constraint bounds are not equal and can be assigned
+    // to -infinity or +infinity. We have to scan the r vector and search a
+    // value j such as b(0, k) <= Sum A(k, R[j]) < b(1, k).
+    //
+    template<typename Xtype>
+    void affect_variables(Xtype& x,
+                          sparse_matrix<int>::row_iterator it,
+                          int k,
+                          int selected,
+                          int r_size,
+                          const Float kappa,
+                          const Float delta) noexcept
+    {
+        if (selected < 0) {
+            for (int i = 0; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, false);
+                P[var->value] -= delta;
+            }
+        } else if (selected + 1 >= r_size) {
+            pi[k] += R[selected].value;
+
+            for (int i = 0; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, true);
+                P[var->value] += delta;
+            }
+        } else {
+            pi[k] += ((R[selected].value + R[selected + 1].value) /
+                      static_cast<Float>(2.0));
+
+            Float d = delta + ((kappa / (static_cast<Float>(1.0) - kappa)) *
+                               (R[selected + 1].value - R[selected].value));
+
+            int i = 0;
+            for (; i <= selected; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, true);
+                P[var->value] += d;
+            }
+
+            for (; i != r_size; ++i) {
+                auto var = it + R[i].id;
+
+                x.set(var->column, false);
+                P[var->value] -= d;
+            }
+        }
+    }
+
+    template<typename Xtype>
+    void push_and_compute_update_row(Xtype& x,
+                                     std::vector<int>::iterator first,
+                                     std::vector<int>::iterator last,
+                                     Float kappa,
+                                     Float delta,
+                                     Float theta,
+                                     Float obj_amp)
+    {
+        for (; first != last; ++first) {
+            auto k = *first;
+
+            if (b[k].min == b[k].max)
+                compute_update_row_01_eq(
+                  x, k, b[k].min, kappa, delta, theta, obj_amp);
+            else
+                compute_update_row_01_ineq(
+                  x, k, b[k].min, b[k].max, kappa, delta, theta, obj_amp);
+        }
+    }
+
+    template<typename Xtype>
+    void compute_update_row(Xtype& x,
+                            std::vector<int>::iterator first,
+                            std::vector<int>::iterator last,
+                            Float kappa,
+                            Float delta,
+                            Float theta)
+    {
+        for (; first != last; ++first) {
+            auto k = *first;
+
+            if (b[k].min == b[k].max)
+                compute_update_row_01_eq(
+                  x, k, b[k].min, kappa, delta, theta, static_cast<Float>(0));
+            else
+                compute_update_row_01_ineq(x,
+                                           k,
+                                           b[k].min,
+                                           b[k].max,
+                                           kappa,
+                                           delta,
+                                           theta,
+                                           static_cast<Float>(0));
+        }
+    }
+};
 
 template<typename Solver,
          typename Float,
