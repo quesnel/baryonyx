5d5cc940cffd7a31c7edb1b92c96075211e15b97
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2019-02-12 15:03:39 +0100

    itm: improbe the best-cycle algorithm

--- lib/test/solve.cpp
+++ lib/test/solve.cpp
@@ -54,7 +54,8 @@ is_essentially_equal(const T v1, const T v2, const T epsilon)
 int
 get_variable(const baryonyx::result& r, std::string variable_name)
 {
-    for (int i = 0, e = static_cast<int>(r.affected_vars.values.size()); i != e;
+    for (int i = 0, e = static_cast<int>(r.affected_vars.values.size());
+         i != e;
          ++i)
         if (r.affected_vars.names[i] == variable_name)
             return i;
@@ -197,7 +198,8 @@ test_preprocessor_2()
     double r;
 
     {
-        auto pb = baryonyx::make_problem(ctx, EXAMPLES_DIR "/capmo1_direct.lp");
+        auto pb =
+          baryonyx::make_problem(ctx, EXAMPLES_DIR "/capmo1_direct.lp");
 
         auto result = baryonyx::solve(ctx, pb);
 
@@ -221,7 +223,8 @@ test_preprocessor_2()
     {
         ss.seekg(0, std::ios::beg);
 
-        auto pb = baryonyx::make_problem(ctx, EXAMPLES_DIR "/capmo1_direct.lp");
+        auto pb =
+          baryonyx::make_problem(ctx, EXAMPLES_DIR "/capmo1_direct.lp");
 
         baryonyx::result result;
         ss >> result;
@@ -277,7 +280,7 @@ test_assignment_problem()
       baryonyx::make_problem(ctx, EXAMPLES_DIR "/assignment_problem_1.lp");
 
     baryonyx::solver_parameters params;
-    params.limit = 50;
+    // params.limit = 50;
     baryonyx::context_set_solver_parameters(ctx, params);
 
     auto result = baryonyx::solve(ctx, pb);
@@ -342,7 +345,7 @@ test_negative_coeff2()
     auto pb = baryonyx::make_problem(ctx, EXAMPLES_DIR "/negative-coeff2.lp");
 
     baryonyx::solver_parameters params;
-    params.limit = 2;
+    // params.limit = 2;
     baryonyx::context_set_solver_parameters(ctx, params);
 
     auto result = baryonyx::solve(ctx, pb);
@@ -571,7 +574,8 @@ test_aim_50_1_6_yes1_2()
 {
     auto ctx = baryonyx::make_context(stdout, 7);
 
-    auto pb = baryonyx::make_problem(ctx, EXAMPLES_DIR "/aim-50-1_6-yes1-2.lp");
+    auto pb =
+      baryonyx::make_problem(ctx, EXAMPLES_DIR "/aim-50-1_6-yes1-2.lp");
 
     baryonyx::solver_parameters params;
     params.limit = -1;
@@ -671,7 +675,7 @@ test_bibd1n()
 #endif
 
 int
-main(int /*argc*/, char* /* argv */[])
+main(int /*argc*/, char* /* argv */ [])
 {
     unit_test::checks("preprocessor", test_preprocessor);
     unit_test::checks("preprocessor_2", test_preprocessor_2);
