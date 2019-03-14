bf966275301c1367a42967867d6e4b5a5b711b64
Gauthier Quesnel <gauthier.quesnel@inra.fr>
2019-03-14 15:00:42 +0100

    core: add random-solver affectation

--- lib/src/private.cpp
+++ lib/src/private.cpp
@@ -113,6 +113,7 @@ context_set_solver_parameters(const context_ptr& ctx,
     ctx->parameters.preprocessor = params.preprocessor;
     ctx->parameters.observer = params.observer;
     ctx->parameters.storage = params.storage;
+    ctx->parameters.solver = params.solver;
 }
 
 solver_parameters
